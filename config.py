ff6be83bba271c0b41d1bdbec08fec8ce016d207
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-08-08 12:55:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ import os, re
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
+OWNER = int(os.environ.get("OWNER", "6318135266"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "modstorexd")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001925329161"))
 
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_NAME = os.environ.get("DB_NAME", "pyro")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 
 
