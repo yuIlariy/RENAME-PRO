148b8d5a7b6174ca7667e25c44e0a6e6c2653289
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-08-08 13:08:16 +0300

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -19,7 +19,7 @@ async def about(bot,update):
 async def donatecm(bot,message):
 	text = script.DONATE_TXT
 	keybord = InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/Kirodewal"), 
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/xspes"), 
         			InlineKeyboardButton("✖️ Close",callback_data = "cancel") ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
@@ -68,13 +68,13 @@ async def donate(bot,update):
 
 @Client.on_callback_query(filters.regex('home'))
 async def home_callback_handler(bot, query):
-    text = f"""Hello {query.from_user.mention} \n\n➻ This Is An Advanced And Yet Powerful Rename Bot.\n\n➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.\n\n➻ You Can Also Convert Video To File Aɴᴅ File To Video.\n\n➻ This Bot Also Supports Custom Thumbnail And Custom Caption.\n\n<b>Bot Is Made By @HxBots</b>"""
+    text = f"""Hello {query.from_user.mention} \n\n➻ This Is An Advanced And Yet Powerful Rename Bot.\n\n➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.\n\n➻ You Can Also Convert Video To File Aɴᴅ File To Video.\n\n➻ This Bot Also Supports Custom Thumbnail And Custom Caption.\n\n<b>Powered By @xspes</b>"""
     keybord = InlineKeyboardMarkup([  
                     [InlineKeyboardButton("📢 Updates", url="https://t.me/HxBots"),
                     InlineKeyboardButton("💬 Support", url="https://t.me/HxSupport")],
                     [InlineKeyboardButton("🛠️ Help", callback_data='help'),
-		            InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
-                    [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://t.me/Kirodewal")]
+		            InlineKeyboardButton("🌌 About", callback_data='about')],
+                    [InlineKeyboardButton("🌋 Owner ☄️", url="https://t.me/xspes")]
 		  ])
     await query.message.edit_text(text=text, reply_markup=keybord)
 
