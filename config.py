1fd198c993f9d569e79b1f47edee23611c8666d6
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-08-08 12:56:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ FORCE_SUBS = os.environ.get("FORCE_SUBS", "modstorexd")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001925329161"))
 
 DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "pyro")
+DB_NAME = os.environ.get("DB_NAME", "pyros")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
